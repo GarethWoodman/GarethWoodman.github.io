7e157c1df8cd4e495ca741307a832cbf03500ab8
GarethWoodman <gareth.woodman92@gmail.com>
2020-07-08 12:36:29 +0100

    Refactored Jquery selectors in one line and shortened conditions

--- bowling-challenge/src/interface.js
+++ bowling-challenge/src/interface.js
@@ -2,79 +2,25 @@ $(document).ready(function() {
   var scoreBoard = new ScoreBoard();
   updateScoreBoard();
 
-  console.log("active");
+  $('#0').on('click', function()  { updateScoreBoard(0) })
+  $('#1').on('click', function()  { updateScoreBoard(1) })
+  $('#2').on('click', function()  { updateScoreBoard(2) })
+  $('#3').on('click', function()  { updateScoreBoard(3) })
+  $('#4').on('click', function()  { updateScoreBoard(4) })
+  $('#5').on('click', function()  { updateScoreBoard(5) })
+  $('#6').on('click', function()  { updateScoreBoard(6) })
+  $('#7').on('click', function()  { updateScoreBoard(7) })
+  $('#8').on('click', function()  { updateScoreBoard(8) })
+  $('#9').on('click', function()  { updateScoreBoard(9) })
+  $('#10').on('click', function() { updateScoreBoard(10) })
 
-  $('#0').on('click', function() {
-    console.log("button 0");
-    scoreBoard.addScore(0);
-    updateScoreBoard();
-  })
+  function updateScoreBoard(score) {
+    scoreBoard.addScore(score);
 
-  $('#1').on('click', function() {
-    console.log("button 1");
-    scoreBoard.addScore(1);
-    updateScoreBoard();
-  })
-
-  $('#2').on('click', function() {
-    console.log("button 2");
-    scoreBoard.addScore(2);
-    updateScoreBoard();
-  })
-
-  $('#3').on('click', function() {
-    console.log("button 3");
-    scoreBoard.addScore(3);
-    updateScoreBoard();
-  })
-
-  $('#4').on('click', function() {
-    console.log("button 4");
-    scoreBoard.addScore(4);
-    updateScoreBoard();
-  })
-
-  $('#5').on('click', function() {
-    console.log("button 5");
-    scoreBoard.addScore(5);
-    updateScoreBoard();
-  })
-
-  $('#6').on('click', function() {
-    console.log("button 6");
-    scoreBoard.addScore(6);
-    updateScoreBoard();
-  })
-
-  $('#7').on('click', function() {
-    console.log("button 7");
-    scoreBoard.addScore(7);
-    updateScoreBoard();
-  })
-
-  $('#8').on('click', function() {
-    console.log("button 8");
-    scoreBoard.addScore(8);
-    updateScoreBoard();
-  })
-
-  $('#9').on('click', function() {
-    console.log("button 9");
-    scoreBoard.addScore(9);
-    updateScoreBoard();
-  })
-
-  $('#10').on('click', function() {
-    console.log("button 10");
-    scoreBoard.addScore(10);
-    updateScoreBoard();
-  })
-
-  function updateScoreBoard() {
     for (var i = 0; i < 10; i++) {
       var frame = scoreBoard.frames[i]
-      if (i === 9) { _updateFinalFrame(frame) }
       $('#frame-' + i + '-first-roll').text(frame.first);
+      if (i === 9) { _updateFinalFrame(frame) }
       if(!frame.hasStrike()) $('#frame-' + i + '-second-roll').text(frame.second);
       $('#frame-' + i + '-score').text(frame.displayScore());
     };
@@ -82,7 +28,6 @@ $(document).ready(function() {
   };
 
   function _updateFinalFrame(frame) {
-    $('#frame-9-first-roll').text(frame.first);
     $('#frame-9-second-roll').text(frame.second);
     if(frame.hasSpare()) { $('#frame-9-third-roll').text(frame.third) }
     $('#frame-9-score').text(frame.displayScore());
@@ -96,17 +41,13 @@ $(document).ready(function() {
         var difference = (10 - currentFrame.second) + 1
         return _disableButtons(difference)
       }
-      if (currentFrame.hasSpare() || currentFrame.first == null )  {
-        return _enableButtons()
-      }
+      if (currentFrame.hasSpare()) { return _enableButtons() }
     }
 
     if (currentFrame.first != null && !currentFrame.hasStrike()) {
       var difference = (10 - currentFrame.first) + 1
       _disableButtons(difference)
-    } else {
-      _enableButtons();
-    }
+    } else { _enableButtons() }
   }
 
   function _enableButtons() {
@@ -116,5 +57,4 @@ $(document).ready(function() {
   function _disableButtons(difference) {
     for (var i = difference; i <= 10; i++) $('#' + i).prop('disabled', true);
   }
-
 });
